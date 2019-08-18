f09177cfda7cf2c878c6368d7eae65c6542b78b0
egardea <erik1gardea@gmail.com>
2019-08-18 16:27:17 -0500

    const, let, var

---
+++ javascript/const_let_var.js
@@ -0,0 +1,20 @@
+function example () {
+    if(!false){
+        var ex = 5;
+    }
+    return ex
+}
+
+console.log(example());
+
+//const conEx = 5;
+//conEx = 6;
+//console.log(conEx); 
+
+const conEx2 = [];
+conEx2.push(2);
+console.log(conEx2);
+
+const conEx3 = {};
+conEx3.name = 'erik';
+console.log(conEx3);
