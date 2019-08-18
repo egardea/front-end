c18fee8e6ba1ef7e1efd0603b0fd0908217b8440
egardea <erik1gardea@gmail.com>
2019-08-18 18:33:01 -0500

    object notation

---
+++ javascript/objectNotations.js
@@ -0,0 +1,13 @@
+const user = {
+    firstName: 'erik',
+    middleName: 'gardea',
+    lastName: 'raul',
+    nickName: 'noclue'
+};
+
+//dot notation
+console.log(user.firstName);
+
+//bracket notation
+console.log(user['firstName']);
+
