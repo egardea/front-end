3d6bbf630d2747579fc51c79f16f659c766bb91e
egardea <erik1gardea@gmail.com>
2019-08-18 17:43:46 -0500

    coercion

---
+++ javascript/coercion.js
@@ -0,0 +1,11 @@
+//explicit vs implicit coercion
+
+//this is implicit coercion
+const ex1 = 2 + 3 + "7";
+console.log(ex1);
+console.log(typeof ex1);
+
+//this explicit coercion
+console.log(typeof String(5))
+console.log(typeof Boolean(0));
+console.log(typeof Number('5'))
