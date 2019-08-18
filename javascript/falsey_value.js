a2aed051f12e6fee3f4a479ab62f26d035be4167
egardea <erik1gardea@gmail.com>
2019-08-18 17:07:35 -0500

    falsey

---
+++ javascript/falsey_value.js
@@ -0,0 +1,14 @@
+//falsey values are: undefined, null, NaN, false, 0, "",
+const ex1 = undefined;
+const ex2 = null;
+const ex3 = NaN;
+const ex4 = false;
+const ex5 = 0;
+const ex6 = "";
+
+console.log(!!ex1);
+console.log(!!ex2);
+console.log(!!ex3);
+console.log(!!ex4);
+console.log(!!ex5);
+console.log(!!ex6);
